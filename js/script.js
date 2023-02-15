adca4e2f49c459f2b1180205d9af99b26a50fc69
AleksanderKasprzak <123496024+OloManolo95@users.noreply.github.com>
2023-02-15 11:05:20 +0100

    add editorconfig, eslintrc, stylelintrc and prepare function generateTags with comments

--- js/script.js
+++ js/script.js
@@ -50,7 +50,8 @@
 
   const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+  optTitleListSelector = '.titles'
+  optArticleTagsSelector = '.post-tags .list';
 
   function generateTitleLinks(){
 
@@ -97,3 +98,31 @@
 }
 
 generateTitleLinks();
+
+function generateTags(){
+  /* find all articles */
+
+  /* START LOOP: for every article: */
+
+    /* find tags wrapper */
+
+    /* make html variable with empty string */
+
+    /* get tags from data-tags attribute */
+
+    /* split tags into array */
+
+    /* START LOOP: for each tag */
+
+      /* generate HTML of the link */
+
+      /* add generated code to html variable */
+
+    /* END LOOP: for each tag */
+
+    /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+}
+
+generateTags();
